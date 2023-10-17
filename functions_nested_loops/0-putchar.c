d1b1a437cebfaf907d15190ee88630b7ec2d4d6c
harishammache <hammache_haris@hotmail.fr>
2023-10-17 01:57:40 -0700

    modify code

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,22 +4,21 @@
 /**
  * main - Entry point
  *
- * Description: 'Write a program that prints _putchar, followed by a new line.'
- *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	 putchar('_');
-	 putchar('p');
-	 putchar('u');
-	 putchar('t');
-	 putchar('c');
-	 putchar('h');
-	 putchar('a');
-	 putchar('r');
-	 putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
 
 	return (0);
 }
