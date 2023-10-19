4a26675f8cf964c0b71df3b446c0e8c8a0ad5726
harishammache <hammache_haris@hotmail.fr>
2023-10-19 07:14:15 -0700

    draws a straight line in the terminal.

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: caractere checked
+ *
+ * Return= n
+*/
+void print_line(int n)
+{
+	int a;
+	
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
