7e29d6d0d250bf7734eb217a760a365ed0d8c1a1
harishammache <hammache_haris@hotmail.fr>
2023-10-19 11:47:00 -0700

    hhhh

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ * @n: caractere checked
+*/
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+				if (a == b)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+
+
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
