6326018243efaa0f8e7b0b11c44f0756efcebaea
harishammache <hammache_haris@hotmail.fr>
2023-10-19 06:21:46 -0700

    hhh

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,19 @@ void print_square(int size)
 	int a;
 	int b;
 
-	for (a = 0; a < size; a++)
-	{
-		for (b = 0; b < size; b++)
+	if (size > 0)
+	{	
+		for (a = 0; a < size; a++)
 		{
-			_putchar('#');
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
