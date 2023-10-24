d1703d4abedc64adc53f230fc2d70b432d5694f4
harishammache <hammache_haris@hotmail.fr>
2023-10-24 03:59:16 -0700

    every other character of a string, starting with the first character

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,18 @@
 */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
+	int j;
 
-	for (i = 0; i <= 9; i++)
+	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (j % 2 == 0)
 		{
-			_putchar(str[i]);
+			_putchar(str[j]);
 		}
 	}
 	_putchar('\n');
