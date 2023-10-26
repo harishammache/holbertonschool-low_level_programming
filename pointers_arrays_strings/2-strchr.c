4a27c705afc81f35e5000ad5ee4b6c467fac0c0b
harishammache <hammache_haris@hotmail.fr>
2023-10-26 07:23:23 -0700

    hhh

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,18 +11,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; )
+	for (i = 0; ;i++)
 	{
 		if (s[i] == c)
 		{
-			return (s);
+			return (s + i);
 		}
 		if (c == ('\0'))
 		{
-			s[i] = c;
-			return (s);
+			return (NULL);
 		}
-		s++;
 	}
-	return (NULL);
 }
