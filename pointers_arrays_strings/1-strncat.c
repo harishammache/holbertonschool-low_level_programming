e95610a7f1bdd8d5b1bae0a1419c1007f20b5dbf
harishammache <hammache_haris@hotmail.fr>
2023-10-29 09:30:36 -0700

    concatenates two strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,5 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string
- * @s: pointeur strlen.
- *
- * Return: lenght (success)
-*/
-int _strlen(char *s)
-{
-	int lenght = 0;
-
-	while (*(s + lenght) != '\0')
-	{
-		lenght++;
-	}
-	return (lenght);
-}
-
-/**
  * _strncat - concatenates two strings
  * @dest: first pointeur
  * @src: second pointeur
@@ -26,14 +9,20 @@ int _strlen(char *s)
 */
 char *_strncat(char *dest, char *src, int n)
 {
+	int j = 0;
 	int i = 0;
-	int j = _strlen(dest);
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (dest[i] != '\0')
 	{
-		dest[j + i] = src[i];
+		i++;
 	}
-	dest[j + i] = '\0';
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
