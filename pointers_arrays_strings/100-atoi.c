9c332cfab086508e1c60434bd0e028671658be5f
harishammache <hammache_haris@hotmail.fr>
2023-10-29 11:29:43 -0700

    hhh

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,14 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int signe = 1;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	for (i = 0; s[i] == '-' || s[i] == '+'; i++)
 	{
-        	if (s[i] == '-')
+		if (s[i] == '-')
 		{
 			signe *= -1;
 		}
