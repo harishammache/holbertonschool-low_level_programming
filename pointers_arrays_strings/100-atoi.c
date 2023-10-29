ed64ba7e89452713d5f94783590bd47a170d8f13
harishammache <hammache_haris@hotmail.fr>
2023-10-29 05:51:31 -0700

    convert a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: first pointeur
+ *
+ * Return: if no numbers in the string, the function must return 0.
+*/
+int _atoi(char *s)
+{
+	int n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (// determiner si + ou -)
+		{
+		}
+		if (// determiner si c un chiffre)
+		{
+			n+ // le chiffre que l'on vient de trouver
+		}
+	}	
+}
