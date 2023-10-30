e6ac8f40ab653dba0da708c4bde05f62bac80009
harishammache <hammache_haris@hotmail.fr>
2023-10-30 07:24:06 -0700

    returns the natural square root of a number.

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * function - new function for condition
+ * @a: first variable
+ * @b: second variable
+ * Return: -1  if no natural square root
+*/
+int function(int a, int b)
+{
+	if (b * b ==  a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (function(a, b + 1));
+	}
+}
+/**
+ *  _sqrt_recursion -  returns the natural square root of a number
+ *  @n: first variable
+ *
+ *  Return: -1 if no natural square root
+*/
+int _sqrt_recursion(int n)
+{
+	return (function(n, 1));
+}
