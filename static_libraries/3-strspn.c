cc50f9b9d2bbd7e4282726ee09cff8cf7f0a2b47
harishammache <hammache_haris@hotmail.fr>
2023-10-31 01:30:43 -0700

    gets the length of a prefix substring

---
+++ static_libraries/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn -  gets the length of a prefix substring
+ * @s: first pointeur
+ * @accept: second pointeur
+ *
+ * Return: s (success)
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int lengthchaine;
+
+	for (lengthchaine = 0; s[lengthchaine] != ('\0'); lengthchaine++)
+	{
+		int lengthsouschaine;
+		int lenght = 0;
+
+		for (lengthsouschaine = 0; accept[lengthsouschaine] != ('\0');
+			lengthsouschaine++)
+		{
+			if (s[lengthchaine] == accept[lengthsouschaine])
+			{
+				lenght = 1;
+				break;
+			}
+		}
+		if (!lenght)
+		{
+			break;
+		}
+	}
+	return (lengthchaine);
+}
