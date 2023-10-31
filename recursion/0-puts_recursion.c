14361adaed736aa047ce587a9adc0ef6e8c21006
harishammache <hammache_haris@hotmail.fr>
2023-10-31 03:16:11 -0700

    prints a string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,15 +5,13 @@
 */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar(s[i]);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 }
