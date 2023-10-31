f14d8855ce033d6bbef53d8c1b06a595718f426d
harishammache <hammache_haris@hotmail.fr>
2023-10-31 01:22:37 -0700

    hhh

---
+++ static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: pointeur
+ * @c: caractere
+ *
+ * Return: c or 0
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; ; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+		if (s[i] == ('\0'))
+			return (NULL);
+	}
+}
