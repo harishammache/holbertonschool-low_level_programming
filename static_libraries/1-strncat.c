a2febee252f522c2d4190949d811ee2e4fa665dc
harishammache <hammache_haris@hotmail.fr>
2023-10-31 01:20:45 -0700

    concatenates two strings

---
+++ static_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: first pointeur
+ * @src: second pointeur
+ * @n: caractere checked
+ *
+ * Return: dest (success)
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int j = 0;
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
