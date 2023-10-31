24eeed96a0cbd51ac96cb8b6f26d70e23a6e3a8b
harishammache <hammache_haris@hotmail.fr>
2023-10-31 01:35:57 -0700

    locates a substring

---
+++ static_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr -  locates a substring.
+ * @haystack: pointeur one
+ * @needle : pointeur second
+ *
+ * Return: 0 is no found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int chaine;
+	int souschaine;
+
+	for (chaine = 0; haystack[chaine] != '\0'; chaine++)
+	{
+		for (souschaine = 0; needle[souschaine] != '\0'
+				&& haystack[chaine + souschaine]
+				== needle[souschaine]; souschaine++)
+		{
+
+		}
+		if (needle[souschaine] == '\0')
+		{
+			return (haystack + chaine);
+		}
+	}
+	return (NULL);
+}
