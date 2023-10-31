5cf8b9f8eae8b4ca951225e59aa4353a1b792bdb
harishammache <hammache_haris@hotmail.fr>
2023-10-31 01:25:16 -0700

    copies a string

---
+++ static_libraries/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: first pointeur
+ * @src: second pointeur
+ * @n: caractere
+ *
+ * Return: dest (success)
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
