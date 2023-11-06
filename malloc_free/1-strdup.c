5b1b7b2bdca6a55af5ee92c1b584f572727815ba
harishammache <hammache_haris@hotmail.fr>
2023-11-06 03:47:40 -0800

    pointer to a newly allocated space in memory

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,16 +13,15 @@ char *_strdup(char *str)
 	int i;
 	int size = 0;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	while (str[size] != '\0')
 	{
 		size++;
 	}
 	str_copie = malloc((size + 1) * sizeof(char));
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; i < size; i++)
 	{
 		str_copie[i] = str[i];
