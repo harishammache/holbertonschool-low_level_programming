381ea7e8053ed427fd5e5dc7ad240a158e4c0a02
harishammache <hammache_haris@hotmail.fr>
2023-11-06 01:35:24 -0800

    creates an array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * *create_array - creates an array of chars
+ * @size: variable use for know the size of unity
+ * @c: second variable for know the number of element
+ *
+ * Return: NULL if size = 0 or pointer if success
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *array_of_char;
+	unsigned int i;
+
+	array_of_char = malloc((size) * sizeof(char));
+
+	if (array_of_char == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array_of_char[i] = c;
+	}
+	return (array_of_char);
+}
