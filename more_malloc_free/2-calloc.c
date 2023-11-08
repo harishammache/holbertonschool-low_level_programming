5237ef95d1b6080a6c771e8eb066ddfcb60225df
harishammache <hammache_haris@hotmail.fr>
2023-11-08 13:15:48 +0000

    allocates memory for an array

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,15 +10,20 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *allocates_array;
+	unsigned int index;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	allocates_array = malloc(sizeof(char) * (nmemb + 1));
+	allocates_array = malloc(size * nmemb);
 	if (allocates_array == NULL)
 	{
 		return (NULL);
+	}
+	for (index = 0; index < size * nmemb; index++)
+	{
+		allocates_array[index] = 0;
 	}
 	return (allocates_array);
 }
