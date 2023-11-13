5c5be68662faf9abaccc2d67542e2831478a5212
harishammache <hammache_haris@hotmail.fr>
2023-11-13 10:54:43 +0000

    new prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @name: second parameter
  * @owner: third parameter
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
