dcd874e1f380994ffa2729fbe4870f4011e6cd72
harishammache <hammache_haris@hotmail.fr>
2023-11-14 13:42:56 +0000

    should contain the 5 following functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_add - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_add - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_add - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_add - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+int op_div(int a, int b)
+{
+	return (a % b);
+}
