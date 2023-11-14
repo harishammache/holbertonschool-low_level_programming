8334e0452299a95aa1aaa97490619213d50b79e3
harishammache <hammache_haris@hotmail.fr>
2023-11-14 11:19:08 +0000

    prints a name.

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name -  prints a name
+ * @name: pointeur
+ * @f: function poiteur
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
