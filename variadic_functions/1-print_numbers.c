fb387c6b26cfb293b8bcaff4f019609e97ba6afb
harishammache <hammache_haris@hotmail.fr>
2023-11-15 13:00:01 +0000

    function that prints numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,13 +19,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	for (index = 0; index < n; index++)
 	{
-		if (index == n - 1)
+		if (index < n - 1)
 		{
 			printf("%d", va_arg(ap, int));
+			printf("%s", separator);
 		}
 		else
 		{
-			printf("%d, ", va_arg(ap, int));
+			printf("%d", va_arg(ap, int));
 		}
 	}
 
