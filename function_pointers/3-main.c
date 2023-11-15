177f4eb06579c7e29bfe4a2852800a2118d54ec0
harishammache <hammache_haris@hotmail.fr>
2023-11-15 09:24:45 +0000

    delete inutil conditoon$

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *op;
+	char *operateur;
 
 	if (argc != 4)
 	{
@@ -21,23 +21,23 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
+	operateur = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (get_op_func(operateur) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*operateur == '/' && num2 == 0) ||
+	    (*operateur == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", get_op_func(operateur)(num1, num2));
 
 	return (0);
 }
