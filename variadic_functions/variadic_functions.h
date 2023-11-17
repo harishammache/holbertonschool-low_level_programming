3302c07fef2d777b368fcba8957d72bfe92db374
harishammache <hammache_haris@hotmail.fr>
2023-11-17 09:15:14 +0000

    i have add typedef

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,22 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct op - new struct type defining a printer.
+ * @op: representing a data type.
+ * @f: function pointers
+ */
+typedef struct op
+{
+	char *ap;
+	void (*f)(va_list arg);
+
+} op_t;
 
 #endif
