5ef08a0ce877b80511ac1193869c1fbb851c4852
harishammache <hammache_haris@hotmail.fr>
2023-11-29 14:18:05 +0000

    change the free

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -43,6 +43,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp->next = new_node;
 	new_node->next->prev = new_node;
 
-	free(new_node);
 	return (new_node);
+	free(new_node);
 }
