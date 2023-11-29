4523cbc75f4bd3b2f2b71bb2a46fe1500ad4679c
harishammache <hammache_haris@hotmail.fr>
2023-11-29 09:03:47 +0000

    i have add the condition

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,17 +12,21 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	if (head != NULL)
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->n = n;
-		new_node->next = *head;
-		*head = new_node;
+		return (NULL);
 	}
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+
 	return (new_node);
 	free(new_node);
 }
