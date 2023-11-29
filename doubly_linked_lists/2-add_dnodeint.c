0907f667e2f39bbaf1d8a52e576476be046a0c53
harishammache <hammache_haris@hotmail.fr>
2023-11-29 08:52:52 +0000

    i have change the structure

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,17 +12,17 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (head != NULL)
 	{
-		return (NULL);
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
 	}
-	new_node->prev = NULL;
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->next = *head;
-	*head = new_node;
 	return (new_node);
 	free(new_node);
 }
