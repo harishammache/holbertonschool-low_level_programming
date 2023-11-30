72894e125c99038a51ed7f7b22c580b97b2f49f8
harishammache <hammache_haris@hotmail.fr>
2023-11-30 14:38:33 +0000

    add a condition

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 	}
 	new_node = malloc(sizeof(dlistint_t));
+	while(idx != 1)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+		{
+			return (NULL);
+		}
+		idx--;
+	}
 	if (new_node == NULL)
 	{
 		return (NULL);
