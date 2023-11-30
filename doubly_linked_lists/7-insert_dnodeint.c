215a2dd374b04e17f2a2718a6b0c7ade100209a9
harishammache <hammache_haris@hotmail.fr>
2023-11-30 14:10:55 +0000

    change the condition and call the fonction

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,30 +12,20 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp;
-	unsigned int index = 0;
 
 	temp = *h;
-
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	if (temp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	if (*h == NULL)
-	{
-		*h = new_node;
-	}
-	else
-	{
-		while (index < idx && temp != NULL)
-		{
-			temp = temp->next;
-			index++;
-		}
-		temp->next = new_node;
 	}
 	new_node->n = n;
 	new_node->prev = temp;
@@ -44,5 +34,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next->prev = new_node;
 
 	return (new_node);
-	free(new_node);
 }
