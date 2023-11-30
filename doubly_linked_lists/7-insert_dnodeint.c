72e9e52c638538551e8d70bc931223237b158d6d
harishammache <hammache_haris@hotmail.fr>
2023-11-30 14:40:56 +0000

    change the place of condition

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,11 +18,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	if (temp->next == NULL)
-	{
-		return (add_dnodeint_end(h, n));
-	}
-	new_node = malloc(sizeof(dlistint_t));
 	while(idx != 1)
 	{
 		temp = temp->next;
@@ -32,6 +27,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
+	if (temp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
