373b14dce96dac6b187f5423443852fb4fa789f4
harishammache <hammache_haris@hotmail.fr>
2023-11-30 14:41:53 +0000

    betty problème

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	while(idx != 1)
+	while (idx != 1)
 	{
 		temp = temp->next;
 		idx--;
