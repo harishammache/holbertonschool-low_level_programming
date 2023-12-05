da0e6b4a25380cc77cb89cf6d08955fe7c68ccc6
harishammache <hammache_haris@hotmail.fr>
2023-12-05 19:22:14 +0000

    betty problème

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,7 +5,8 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: ilename is the name of the file
- * @tex_content: text_content is the NULL terminated string to add at the end of the file
+ * @text_content: text_content is the NULL terminated
+ * string to add at the end of the file
  *
  * Return: 1 on success and -1 on failure
 */
@@ -20,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	o_file = open(filename, O_APPEND | O_WRONLY);
-	if(o_file == -1)
+	if (o_file == -1)
 	{
 		return (-1);
 	}
