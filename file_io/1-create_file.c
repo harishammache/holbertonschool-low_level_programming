817dfb8e758bf9aa2047bf65ca361c4938b33be7
harishammache <hammache_haris@hotmail.fr>
2023-12-05 14:06:09 +0000

    change permission

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	o_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	o_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (o_file == -1)
 	{
 		return (-1);
