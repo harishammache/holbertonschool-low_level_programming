6ef88b7039a501a273a79603b1d376bf229761fc
harishammache <hammache_haris@hotmail.fr>
2023-12-06 11:54:04 +0000

    betty problème

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,5 +1,9 @@
 #ifndef HASH_TABLES
 #define HASH_TABLES
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -10,9 +14,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 /**
  * struct hash_table_s - Hash table data structure
@@ -24,8 +28,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	 unsigned long int size;
+	 hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
