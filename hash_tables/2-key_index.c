c129e23bfb715b891da3b3110e2b0aefcdd17e9d
harishammache <hammache_haris@hotmail.fr>
2023-12-06 12:18:45 +0000

    betty problèm

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -6,7 +6,8 @@
  * @key: key
  * @size: size of te array of the hash table
  *
- * Return: index at which the key/value pair should be stored in the array of the hash table
+ * Return: index at which the key/value pair
+ *			should be stored in the array of the hash table
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
