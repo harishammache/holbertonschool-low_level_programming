6c14097a599eb258d9cef003af8aaa741c3617b1
harishammache <hammache_haris@hotmail.fr>
2023-12-06 12:20:23 +0000

    add ()

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -11,9 +11,9 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = 0;
+	unsigned long int index;
 
-	index = hash_djb2(key) % size;
+	index = (hash_djb2(key)) % size;
 
 	return (index);
 }
